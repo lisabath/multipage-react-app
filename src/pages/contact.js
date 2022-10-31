f5b092c9bfd47cd61df16d32a092682652a6f8f2
Taliha <103150201+talihaq@users.noreply.github.com>
2022-10-31 13:49:03 +0000

    Add Form component to contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,17 @@
+import Form from "../components/Form";
+
 function Contact() {
     return (
-      <div className="contact">
-        <h1>Contact</h1>
-        <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore aliquid deserunt ut minus iste dolore, atque et corrupti eum incidunt doloribus consectetur sunt, odio doloremque natus deleniti dolor ab similique.
-        </p>
-      </div>
+      <>
+        <div className="contact contact-page-background">
+          <h1>Contact</h1>
+          <p>
+            Use this contact form to get in touch with us.
+          </p>
+          <Form />
+        </div>
+
+      </>
     );
   }
   
