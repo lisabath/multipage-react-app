5fb5ce9b3a708f11a1c049f9fd8f96002a719658
lisabath <112872071+lisabath@users.noreply.github.com>
2022-10-31 15:55:07 +0000

    menu changes

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,29 +1,51 @@
 function Menu() {
-    return (
-      <div className="menu">
-        <h1>Menu</h1>
-
-        <h2>Set Lunch</h2>
-
-        <p>Two Course £25
-            Three Courses £30</p>
-        <h3>Starters</h3>
-        <p></p>
-        <h3>Main</h3>
-        <p></p>
-        <h3>Sides</h3>
-        <p></p>
-        <h3>Puddings</h3>
-        <p>Tiramasu</p>
-
-        <h2>Drink</h2>
-        <h3></h3>
-
-        <h2>Dinner</h2>
-        niti dolor ab similique.
-       
-      </div>
-    );
-  }
-  
-  export default Menu;
+        return (
+        
+          <div className="menu">
+            <div className="darkoverlay">
+          
+            <h1>Menu</h1>
+    
+            <h2>Set Lunch</h2>
+                <p>Two Course £25</p>
+                <p>Three Courses £30</p>
+    
+            <h3>Starters</h3>
+            <p>Bruschetta al salmone
+    Loch Duart cured salmon, beetroot, jerusalem artichoke, butter sauce</p>
+                <p>Soup of the day, homemade Tuscan bread</p>
+            <h3>Mains</h3>
+            <p>Risotto
+    Butternut squash risotto, walnuts, parmesan, truffle oil (v)
+    Gnocchi alla romana</p>
+    <p>Traditional Roman gnocchi, butternut squash, wild mushroom, crispy guanciale</p>
+    <p>Pollo
+    Corn fed chicken supreme, greens, dauphinoise potato, brandy sauce</p>
+           
+            <h3>Puddings</h3>
+           
+            <p>Panna cotta
+    Lavender panna cotta, apple and plum compote, crumble</p>
+            <p>Gelato o sorbetto
+    Three scoops of our ice cream or sorbet selection (v)</p>
+    <p>Formaggi
+    Choice of Taleggio, Pecorino al fieno, Ubriaco Rosso, Robiola with biscuits, grapes, and Italian pear chutney (v)
+    +6 Supplement for full board</p>
+    
+    <h3>Cocktails</h3>
+           
+           <p>STRAWBERRY & ELDERFLOWER MOJITO | 9.50 | VG V 
+    Bacardi, St-Germain Elderflower Liqueur, strawberry liqueur, strawberry purée, mint</p>
+    <p>SRHUBARB & ROSE | 8.75 | VG V 
+    Lanique Spirit of Rose, rhubarb and sage cordial, rhubarb liqueur and beetroot</p>
+    <p>PEACH AND HONEY BLOSSOM | 8.95 | VG V 
+    Maker’s Mark Whisky, wild peach liqueur, honey, lemon and thyme</p>
+    <p>NON ALCOHOLIC CHERRY BLOSSOM | 5.95 | VG V 
+    Available non alcoholic with Everleaf non alcoholic spirit</p>   
+            </div>
+          </div>
+         
+        );
+      }
+      
+      export default Menu;
